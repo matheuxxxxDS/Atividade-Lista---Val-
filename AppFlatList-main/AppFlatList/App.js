00c1fc8d8220946610685bc9c61d583647440f8a
MatheuxxxxDS <matheushe94@gmail.com>
2024-09-25 21:12:45 -0300

    AA

---
+++ AppFlatList-main/AppFlatList/App.js
@@ -0,0 +1,25 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ListaProdutoScreen from './components/ListaProdutoScreen';
+import DetalheProdutoScreen from './components/DetalheProdutoScreen';
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ListaProduto'>
+        <Stack.Screen name="ListaProduto" component={ListaProdutoScreen} />
+        <Stack.Screen name="DetalheProduto" component={DetalheProdutoScreen} />
+      </Stack.Navigator>         
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ AppFlatList-main/AppFlatList/components/DetalheProdutoScreen.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { Card, Text } from "react-native-paper";
+import Produtos from "../data/Produtos";
+
+const getProdutoPorId = (id) => {
+    return Produtos.find(p => p.id == id)
+}
+
+export default ({ route, navigation }) => {
+    const [produto, setProduto] = useState(getProdutoPorId(route.params.id))
+
+    return (
+        <View>
+            <Card>
+                <Card.Cover source={{
+                    uri: produto.urlImagem
+                }} />
+                <Card.Title title={produto.titulo} />
+                <Card.Content>
+                    <Text variant="bodyMedium">{produto.descricao}</Text>
+                </Card.Content>
+            </Card>
+        </View>
+    )
+}
